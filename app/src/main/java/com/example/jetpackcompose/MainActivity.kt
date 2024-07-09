d2200607d6c9d4b246061bbc3abc20d64ab17c50
TanushreePandaa <tanupanda26@gmail.com>
2024-07-09 20:53:08 +0530

    Day11

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,151 +4,169 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-//import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
-//import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import kotlin.math.floor
+import kotlin.reflect.KProperty
 
-class MainActivity<Bundle> : ComponentActivity() {
-    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+class MainActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
         // enableEdgeToEdge()
         setContent {
-            JetpackComposeTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-                    Column {
-                        Text(text = "Tanushree-Panda", color = Color.Cyan, fontSize = 30.sp, fontStyle = FontStyle.Italic, fontWeight = FontWeight.Bold)
-                        Text("Hello World", color = Color.Blue)
-                        Text("Hello World", fontSize = 30.sp)
-                        val offset = Offset(5.0f, 10.0f)
-                        Text(
-                            text = "Hello world!",
-                            style = TextStyle(
-                                fontSize = 24.sp,
-                                shadow = Shadow(
-                                    color = Color.Blue, offset = offset, blurRadius = 3f
-                                )
-                            )
-                        )
-                        MultipleStylesInText()
-                        TextStyledBrushSnippet("The Text composable has multiple optional parameters to style its content. Below, we’ve listed parameters that cover the most common use cases with text. ")
-//                    TextStyledBrushSnippet()
-                        Text(
-                            text = buildAnnotatedString {
-                                append("Do not allow people to dim your shine\n")
-                                val rainbowColors: List<Color> = listOf(Color.Red, Color.Blue)
-                                withStyle(
-                                    SpanStyle(
-                                        brush = Brush.linearGradient(
-                                            colors = rainbowColors
-                                        )
-                                    )
-                                ) {
-                                    append("because they are blinded.")
-                                }
-                                append("\nTell them to put some sunglasses on.")
-                            }
-                        )
-                    }
+        JetpackComposeTheme {
+            Column {
+                AppNavigation()
+                CounterScreen()
+            }
+
+        }
+    }
+}
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "screen1") {
+        composable("screen1"){Screen1(navController)}
+        composable("screen2"){Screen2(navController)}
+        composable("screen3"){Screen3(navController)}
+        composable("screen4/{data}", arguments = listOf(navArgument("data"){type= NavType.StringType}))
+        {backstackentry ->
+            Screen4(navController, backstackentry.arguments?.getString("data") ?: "a")
+        }
+        composable("screen5"){Screen5(navController)}
+
+    }
+}
+
+@Composable
+fun Screen5(navController: NavHostController){
+    val itemsList = List(100){"item = $it"}
+    Column(modifier = Modifier.fillMaxWidth().padding(20.dp)
+    ) {
+        Button(onClick = { navController.navigate("screen1") }) {
+            Text(text = "Go to screen1")
+        }
+        LazyVerticalGrid(columns = GridCells.Adaptive(120.dp)) {
+            items(itemsList){
+                Card {
+                    Text(text = it)
                 }
             }
         }
-
-//    override fun onStart() {
-//        super.onStart()
-//        println("LifeCycle On Create Started")
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//        println("LifeCycle On Resume Started")
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        println("LifeCycle On Pause Started")
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        println("LifeCycle On Stop Started")
-//    }
     }
 
-//@Composable
-//fun TextStyledBrushSnippet() {
-//    val rainbowColors: List<Color> = listOf()
-//    // [START android_compose_text_annotatedString_brush]
-//    Text(
-//        text = buildAnnotatedString {
-//            append("Do not allow people to dim your shine\n")
-//            withStyle(
-//                SpanStyle(
-//                    brush = Brush.linearGradient(
-//                        colors = rainbowColors
-//                    )
-//                )
-//            ) {
-//                append("because they are blinded.")
-//            }
-//            append("\nTell them to put some sunglasses on.")
-//        }
-//    )
-//    // [END android_compose_text_annotatedString_brush]
-//}
+}
 
-    @Composable
-    fun MultipleStylesInText() {
-        Text(
-            buildAnnotatedString {
-                withStyle(style = SpanStyle(color = Color.Blue)) {
-                    append("H")
-                }
-                append("ello ")
+@Composable
+fun Screen4(navController: NavHostController, data: String?) {
+    Column {
+        Text(text = "This is Screen4  and the data is $data", fontSize = 30.sp, color = Color.Green)
+        Button(onClick = { navController.navigate("screen5") }) {
+            Text(text = "Go to screen5")
+        }
+    }
+}
 
-                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Red)) {
-                    append("W")
-                }
-                append("orld")
-            }
+@Composable
+fun Screen3(navController: NavHostController) {
+    Column {
+        var text by remember {
+            mutableStateOf("")
+        }
+        Text(text = "This is Screen3", fontSize = 30.sp, color = Color.Cyan)
+        OutlinedTextField(value = text, onValueChange = {
+            text = it
+        })
+        Button(onClick = { navController.navigate("screen4/$text") }) {
+            Text(text = "Go to screen4")
+        }
+    }
+}
+
+@Composable
+fun Screen2(navController: NavHostController) {
+    Column {
+        Text(text = "This is Screen2", fontSize = 30.sp, color = Color.Blue)
+        Button(onClick = { navController.navigate("screen3") }) {
+            Text(text = "Go to screen3")
+        }
+    }
+}
+
+
+@Composable
+fun Screen1(navController: NavHostController) {
+    Column {
+        Text(text = "This is Screen1", fontSize = 30.sp, color = Color.Red)
+        Button(onClick = { navController.navigate("screen2") }) {
+            Text(text = "Go to screen2")
+        }
+    }
+}
+
+@Composable
+fun CounterScreen(){
+    var count by remember {
+        mutableIntStateOf(0)
+    }
+    var text by rememberSaveable { mutableStateOf("") }
+    Column(
+        modifier= Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center
+    ){
+        Text(text = "The count is $count")
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = { count++ }) {
+            Text(text = "Increment")
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = { count-- }) {
+            Text(text = "Decrement")
+        }
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text =it },
+            label = { Text("Enter Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
     }
-
-    @Composable
-    fun TextStyledBrushSnippet(text: String) {
-        val lightBlue = Color(0xFF0066FF)
-        val purple = Color(0xFF800080)
-        val gradientColors = listOf(Color.Cyan, lightBlue, purple)
-
-        Text(
-            text = text,
-            style = TextStyle(
-                brush = Brush.linearGradient(
-                    colors = gradientColors
-                )
-            )
-        )
-    }
-
+}
+        }
