167a5d86c2b646a23907eeb36f8a97df0ff67bc4
TanushreePandaa <tanupanda26@gmail.com>
2024-07-19 21:09:36 +0530

    Day18

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,92 +1,69 @@
 package com.example.jetpackcompose
 
 
+import android.net.Uri
 import android.os.Bundle
 import android.service.controls.ControlsProviderService.TAG
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.jetpackcompose.Model.Data.Product
-import com.example.jetpackcompose.ViewModels.ProductViewModel
-import com.example.jetpackcompose.ViewModels.UserViewModel
-import com.example.jetpackcompose.Views.productScreen
-import com.example.jetpackcompose.Views.UserScreen
+import coil.compose.rememberAsyncImagePainter
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 import com.google.firebase.Firebase
-import com.google.firebase.FirebaseException
-import com.google.firebase.FirebaseTooManyRequestsException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
-import com.google.firebase.auth.PhoneAuthCredential
-import com.google.firebase.auth.PhoneAuthOptions
-import com.google.firebase.auth.PhoneAuthProvider
-import com.google.firebase.auth.PhoneAuthProvider.ForceResendingToken
 import com.google.firebase.firestore.firestore
-import com.google.firebase.firestore.toObject
-import kotlinx.coroutines.launch
-import java.util.concurrent.TimeUnit
+import com.google.firebase.storage.storage
+import java.util.UUID
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // enableEdgeToEdge()
         setContent {
-            val userList = remember {
-                mutableStateOf<List<User>>(emptyList())
-            }
-            LaunchedEffect(Unit) {
-                fetchStudents { users ->
-                    userList.value = users
-                }
-            }
+//            val userList = remember {
+//                mutableStateOf<List<User>>(emptyList())
+//            }
+//            LaunchedEffect(Unit) {
+//                fetchStudents { users ->
+//                    userList.value = users
+//                }
+//            }
             JetpackComposeTheme {
                 // AddUserScreen()
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(20.dp)
-                ) {
-                    items(userList.value) { user ->
-                        Text(text = "NAME: ${user.name}, Age: ${user.age}")
-                    }
-                }
+//                LazyColumn(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .padding(20.dp)
+//                ) {
+//                    items(userList.value) { user ->
+//                        Text(text = "NAME: ${user.name}, Age: ${user.age}")
+//                    }
+//                }
+                ImageUploadScreen()
             }
         }
     }
@@ -118,6 +95,54 @@ class MainActivity: ComponentActivity() {
             .addOnFailureListener { e ->
                 Log.w(TAG, "Error getting document", e)
             }
+    }
+
+    val storage = Firebase.storage
+    val storageRef = storage.reference
+    fun uploadImage(uri: Uri, context: android.content.Context) {
+        val fileName = "Images/${UUID.randomUUID()}.jpg"
+        val imageRef = storageRef.child(fileName)
+
+        imageRef.putFile(uri)
+            .addOnSuccessListener { taskSnapshot ->
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    Toast.makeText(context, "Image Uploaded Successfully: $uri", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(context, "Image Uploading Failed: ${e.message}", Toast.LENGTH_SHORT)
+                    .show()
+
+            }
+    }
+
+    @Composable
+    fun ImageUploadScreen() {
+        val context = LocalContext.current
+        val imageUri = remember { mutableStateOf<Uri?>(null) }
+        val launcher =
+            rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+                imageUri.value = uri
+                uri?.let { uploadImage(uri, context) }
+            }
+        Column(
+            modifier = Modifier.padding(20.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = { launcher.launch("image/*") }) {
+                Text("SELECT IMAGE")
+            }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        imageUri.value?.let { uri ->
+            Image(
+                painter = rememberAsyncImagePainter(uri),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
+        }
     }
 
     @Composable
